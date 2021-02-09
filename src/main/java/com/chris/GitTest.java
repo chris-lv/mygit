f97bc0c05e3bcfd4d2810e39f9fa4fa8298ec8f5
chris-lv <485194762@qq.com>
2021-02-09 18:01:48 +0800

    测试提交

---
+++ src/main/java/com/chris/GitTest.java
@@ -0,0 +1,16 @@
+package com.chris;
+
+/**
+ * ClassName:GitTest
+ * Package:com.chris
+ * Description:
+ *
+ * @Date:2021/2/9 17:52
+ * @Author:Chris
+ * @E-mail:gzshs@vip.qq.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Test.......");
+    }
+}
